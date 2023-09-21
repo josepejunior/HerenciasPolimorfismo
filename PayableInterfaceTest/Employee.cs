bc35f0d527476f4a42d788437725e804b3d340bd
Joseph Mendoza <josephmenjunior@gmail.com>
2023-09-21 09:51:06 -0600

    Implementando interfaces

---
+++ PayableInterfaceTest/Employee.cs
@@ -0,0 +1,37 @@
+﻿using PayableInterfaceTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayableInterfaceTest
+{
+    // Employee abstract base class taht implements interface IPayable
+    public abstract class Employee : IPayable
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+        public string SocialSecurityNumber { get; }
+
+        // three-parameter constructor
+        public Employee(string firstname, string lastname,
+            string socialSecurityNumber)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            SocialSecurityNumber = socialSecurityNumber;
+        }
+
+        // return string representation of Employee object, using properties
+        public override string ToString() => $"{FirstName} {LastName}\n" +
+            $"social security number: {SocialSecurityNumber}";
+
+        // abstract method overridden by derived classes
+        public abstract decimal Earnings(); // no implementation here
+
+        // implementing GetPaymentAmount here eneables the entire employee 
+        // class hierarchy to be used in an app that process IPayables
+        public decimal GetPaymentAmount() => Earnings();
+    }
+}
--- PayableInterfaceTest/Program.cs
+++ PayableInterfaceTest/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using PayableInterfaceTest;
+
+// create a list<IPayable> and initialize it whit four
+// objects of classes that implement interface IPayable
+var payableObjects = new List<IPayable>()
+{
+    new Invoice("01234", "seat", 2, 375.00M),
+    new Invoice("56789", "tire", 4, 79.95M),
+    new SalariedEmployee("Carlos", "Guevara", "111-11-1111", 800.00M),
+    new SalariedEmployee("Carlos", "Guevara", "111-11-1111", 800.00M)
+};
+
+Console.WriteLine("Invoice and Employees processed" +
+    "polymorphically:\n");
+
+// generically process each element in payableObjects
+foreach (var payable in payableObjects)
+{
+    Console.WriteLine($"{payable}");
+    Console.WriteLine(
+        $"payment due: {payable.GetPaymentAmount():C}\n");
+}
